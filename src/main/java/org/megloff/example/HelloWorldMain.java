8d6e7c7837a059eb25e09bd49e5cabb7d8fcb82b
egloffmark <info@megloff.ch>
2019-08-14 22:38:37 +0200

    Initial commit

---
+++ src/main/java/org/megloff/example/HelloWorldMain.java
@@ -0,0 +1,11 @@
+package org.megloff.example;
+
+import java.util.Date;
+
+public class HelloWorldMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! " + new Date());
+    }
+
+}
---
+++ start-h2-server.sh
@@ -0,0 +1,1 @@
+java -cp ./h2/bin/h2-1.4.199.jar org.h2.tools.Server -ifNotExists
